e5b297a9569ded022fcda2e3ec7523f52ebffed2
FLAVIO CELESTINO <81762067+punk23@users.noreply.github.com>
2021-06-17 20:04:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Apuracao_votos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            apuracao x;
+            x = new apuracao();
+            double a = 0;
+            double b = 0;
+            double c = 0;
+
+            Console.WriteLine("Informe a quantidade total de eleitores: ");
+            x.totalEleitor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade total de votos válidos: ");
+            x.valido = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade total de votos brancos: ");
+            x.branco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a quantidade total de votos nulos: ");
+            x.nulo = double.Parse(Console.ReadLine());
+            a = (x.valido / x.totalEleitor)*100;
+            Console.WriteLine("Votos válidos: "+a+"%");
+            b = (x.branco / x.totalEleitor)*100;
+            Console.WriteLine("Votos brancos: " + b + "%");
+            c = (x.nulo / x.totalEleitor)*100;
+            Console.WriteLine("Votos nulos: " + c + "%");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ apuracao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apuracao_votos
+{
+    class apuracao
+    {
+        public double totalEleitor, valido, branco, nulo;
+        public apuracao()
+        {
+        }
+
+        public apuracao(double totalEleitor, double valido, double branco, double nulo)
+        {
+            this.totalEleitor = totalEleitor;
+            this.valido = valido;
+            this.branco = branco;
+            this.nulo = nulo;
+        }
+
+    }
+}
